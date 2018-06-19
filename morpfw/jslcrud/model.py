d61d3faa4d5bd78c2a6b316e0ab20ddb3c70b57d
Izhar Firdaus <kagesenshi.87@gmail.com>
2018-06-19 18:24:19 +0800

    Model is now subscriptable to data attribute

--- morpfw/jslcrud/model.py
+++ morpfw/jslcrud/model.py
@@ -139,6 +139,15 @@ class Model(object):
     update_view_enabled = True
     delete_view_enabled = True
 
+    def __setitem__(self, key, value):
+        self.data[key] = value
+
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def __delitem__(self, key):
+        del self.data[key]
+
     @property
     def statemachine_view_enabled(self):
         if self.state_machine():
