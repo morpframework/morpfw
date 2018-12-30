3ea68747c42e574aa4de9afa97e37d76dd99be24
Izhar Firdaus <kagesenshi.87@gmail.com>
2018-12-30 14:48:47 +0800

    added permission rule for auth views

---
+++ morpfw/auth/permission_rule.py
@@ -0,0 +1,17 @@
+from .app import App
+from .user.model import UserModel
+from . import permission as authperm
+
+
+@App.permission_rule(model=UserModel, permission=authperm.ChangePassword)
+def allow_change_password(identity, context, permission):
+    return True
+
+
+@App.permission_rule(model=UserModel, permission=authperm.DeleteUser)
+def allow_delete_user(identity, context, permission):
+    if context.request.app.has_role(context.request, 'administrator'):
+        return True
+    if context['id'] == identity.userid:
+        return True
+    return False
