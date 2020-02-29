d329c92679df0f4091f6ea1e133423ba32352332
Izhar Firdaus <kagesenshi.87@gmail.com>
2020-02-29 22:08:26 +0800

    add namespaced identifier validator

--- morpfw/validator/field.py
+++ morpfw/validator/field.py
@@ -2,6 +2,7 @@ import rulez
 import re
 
 valid_identifier_pattern = re.compile(r'^[a-z][a-z0-9_]*$')
+valid_namespaced_identifier_pattern = re.compile(r'^[a-z][a-z0-9_\.]*$')
 
 def unique(request, schema, field, value, mode=None):
     if mode=='update':
@@ -18,3 +19,12 @@ def valid_identifier(request, schema, field, value, mode=None):
         return (
             "Only lower cased alphanumeric and _ are accepted and shall"
             " not start with number nor _")
+
+
+def valid_namespaced_identifier(request, schema, field, value, mode=None):
+    if not valid_namespaced_identifier_pattern.match(value):
+        return (
+            "Only lower cased alphanumeric, . and _ are accepted, and shall"
+            " not start with number, . nor _")
+
+
