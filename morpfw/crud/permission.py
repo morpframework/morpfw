79e57d3c991491a72176569d01c649e41d1ef731
Izhar Firdaus <kagesenshi.87@gmail.com>
2020-09-03 22:59:21 +0800

    getting Create permission will bring together View permission

--- morpfw/crud/permission.py
+++ morpfw/crud/permission.py
@@ -3,26 +3,20 @@ from ..permission import All as MFWAll
 class All(MFWAll):
     pass
 
-
-class View(All):
+class Create(All):
     pass
 
+class View(Create):
+    pass
 
 class Edit(All):
     pass
 
-
-class Create(All):
-    pass
-
-
 class Delete(All):
     pass
 
-
 class Search(All):
     pass
-
 
 class Aggregate(All):
     pass
