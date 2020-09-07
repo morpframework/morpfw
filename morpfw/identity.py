a247cfe93b28194dc59449f06ea5df1519fab008
Izhar Firdaus <kagesenshi.87@gmail.com>
2020-09-07 14:06:36 +0800

    added convenient accessor for is_administrator

--- morpfw/identity.py
+++ morpfw/identity.py
@@ -1,7 +1,17 @@
-import morepath
 import pytz
 
+import morepath
+
+from .authn.pas.utils import has_role
+
+
 class Identity(morepath.Identity):
+    def __init__(self, request, userid, **kw):
+        self.request = request
+        super().__init__(userid, **kw)
 
     def timezone(self):
         return pytz.UTC
+
+    def is_administrator(self):
+        return has_role(self.request, "administrator", userid=self.userid)
