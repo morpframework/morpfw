06c8e5ecf6cd526eda69f805a4fcf9e51f16fd4d
Izhar Firdaus <kagesenshi.87@gmail.com>
2020-11-19 10:11:44 +0800

    use GUID field from sqlalchemy_utils

--- morpfw/crud/storage/sqlstorage.py
+++ morpfw/crud/storage/sqlstorage.py
@@ -7,6 +7,7 @@ import pytz
 import jsl
 import sqlalchemy as sa
 import sqlalchemy_jsonfield as sajson
+import sqlalchemy_utils as sautils
 from rulez import compile_condition
 from sqlalchemy import func
 from sqlalchemy.dialects.postgresql import UUID
@@ -267,39 +268,7 @@ class SQLStorage(BaseStorage):
         d["deleted"] = datetime.now(tz=pytz.UTC)
 
 
-class GUID(TypeDecorator):
-    """Platform-independent GUID type.
-
-    Uses PostgreSQL's UUID type, otherwise uses
-    CHAR(32), storing as stringified hex values.
-
-    """
-
-    impl = CHAR
-
-    def load_dialect_impl(self, dialect):
-        if dialect.name == "postgresql":
-            return dialect.type_descriptor(UUID())
-        else:
-            return dialect.type_descriptor(CHAR(32))
-
-    def process_bind_param(self, value, dialect):
-        if value is None:
-            return value
-        elif dialect.name == "postgresql":
-            return str(value)
-        else:
-            if not isinstance(value, uuid.UUID):
-                return "%.32x" % uuid.UUID(value).int
-            else:
-                # hexstring
-                return "%.32x" % value.int
-
-    def process_result_value(self, value, dialect):
-        if value is None:
-            return value
-        else:
-            return uuid.UUID(value)
+GUID = sautils.UUIDType
 
 
 class MappedTable(object):
