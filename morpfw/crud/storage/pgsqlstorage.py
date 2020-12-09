6f3842b7337d7a521a9acba20a8eff4217406e4f
Izhar Firdaus <kagesenshi.87@gmail.com>
2020-12-09 12:22:49 +0800

    refactor construct_orm_model for use with SQLStorage

--- morpfw/crud/storage/pgsqlstorage.py
+++ morpfw/crud/storage/pgsqlstorage.py
@@ -17,16 +17,39 @@ from sqlalchemy.types import CHAR, TypeDecorator
 
 from ..app import App
 from .base import BaseStorage
-from .sqlstorage import MappedTable, SQLStorage
+from .sqlstorage import Base, MappedTable, SQLStorage
 
-db_meta = MetaData()
+db_meta = Base.metadata
 
 _mapped_models = {}
 
 
+def construct_orm_model(schema, metadata, *, name=None):
+    existing = _mapped_models.get(schema, None)
+    if existing:
+        return existing
+
+    table = dc2pgsqla.convert(schema, metadata, name=name)
+
+    class Table(MappedTable):
+
+        __table__ = table
+
+    saorm.mapper(Table, table)
+
+    _mapped_models[schema] = Table
+    return Table
+
+
 class PgSQLStorage(SQLStorage):
+    """
+    At the moment this storage only works correctly with EntityContent.
+
+    To use with code level SQLStorage, use `construct_orm_model`
+    """
 
     _temp: dict = {}
+    __table_name__ = None
 
     def __init__(self, request, metadata=None, blobstorage=None):
         super().__init__(request, blobstorage=blobstorage)
@@ -35,17 +58,5 @@ class PgSQLStorage(SQLStorage):
 
     @property
     def orm_model(self):
-        existing = _mapped_models.get(self.model.schema, None)
-        if existing:
-            return existing
-
-        table = dc2pgsqla.convert(self.model.schema, self.metadata)
-
-        class Table(MappedTable):
-
-            __table__ = table
-
-        saorm.mapper(Table, table)
-
-        _mapped_models[self.model.schema] = Table
-        return Table
+        name = getattr(self, "__table_name__", None)
+        return construct_orm_model(self.model.schema, self.metadata, name=name)
--- morpfw/sql.py
+++ morpfw/sql.py
@@ -1,1 +1,2 @@
-from .crud.storage.sqlstorage import Base, GUID
+from .crud.storage.pgsqlstorage import construct_orm_model
+from .crud.storage.sqlstorage import GUID, Base
